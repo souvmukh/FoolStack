c191d4c90fcaeaa7df8d9058db0f944c6594d85c
souvmukh <souvmukh@users.noreply.github.com>
2022-01-26 12:41:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,jsonify,request
 from flask_restful import Resource, Api
 from makesound import MakeSound
 
+#1. To Save your Sound 2. Exception Handling API
+
 app = Flask(__name__)
 api = Api(app)
 
